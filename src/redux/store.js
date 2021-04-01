45d05a8fb54fc89acccce8f1262302399a810ba4
Sindu Andita <sinduandita@gmail.com>
2021-04-01 14:56:26 +0700

    setting process environment

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,9 +4,13 @@ import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [logger];
+const middlewares = [];
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
 
 export const store = createStore(
   rootReducer,
